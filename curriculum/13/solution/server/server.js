09d7675a3badcc446af59f75830cb11785faf020
Allie <allie@codefellows.com>
2018-07-05 11:41:11 -0700

    Updating SQL syntax for class 13 solution code

--- curriculum/13/solution/server/server.js
+++ curriculum/13/solution/server/server.js
@@ -8,7 +8,6 @@ const pg = require('pg');
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
-const TOKEN = parseInt(process.env.TOKEN);
 
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -21,56 +20,46 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 // API Endpoints
-app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)));
+app.get('/api/v1/books', (request, response) => {
+  fetchAllBooks()
+    .then(results => response.send(results.rows))
+    .catch(console.error);
+});
 
-// API Endpoints
-app.get('/api/v1/books', (req, res) => {
+app.get('/api/v1/books/:id', (request, response) => {
+  fetchOneBook(request.params.id)
+    .then(results => response.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', (request, response) => {
+  addNewBook(request.body)
+    .then(response.sendStatus(201))
+    .catch(console.error);
+});
+
+
+app.get('*', (request, response) => response.status(403).send('This route does not exist'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+function fetchAllBooks() {
   let SQL = 'SELECT book_id, title, author, image_url, isbn FROM books;';
+  
+  return client.query(SQL);
+}
 
-  client.query(SQL)
-    .then(results => res.send(results.rows))
-    .catch(console.error);
-});
-
-app.get('/api/v1/books/:id', (req, res) => {
+function fetchOneBook(bookId) {
   let SQL = 'SELECT * FROM books WHERE book_id=$1;';
-  let values = [req.params.id];
+  let values = [bookId];
 
-  client.query(SQL, values)
-    .then(results => res.send(results.rows))
-    .catch(console.error);
-});
+  return client.query(SQL, values);
+}
 
-app.post('/api/v1/books', (req, res) => {
-  let {title, author, isbn, image_url, description} = req.body;
+function addNewBook(newBook) {
+  let {title, author, isbn, image_url, description} = newBook;
   let SQL = 'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);';
 
   let values = [title, author, isbn, image_url, description];
-  client.query(SQL, values)
-    .then(res.sendStatus(201))
-    .catch(console.error);
-});
-
-app.put('/api/v1/books/:id', (req, res) => {
-  let {title, author, isbn, image_url, description} = req.body;
-
-  let SQL = 'UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE book_id=$6;';
-  let values = [title, author, isbn, image_url, description, req.params.id];
-
-  client.query(SQL, values)
-    .then(() => res.sendStatus(204))
-    .catch(console.error)
-});
-
-app.delete('/api/v1/books/:id', (req, res) => {
-  let SQL = 'DELETE FROM books WHERE book_id=$1';
-  let values = [req.params.id];
-
-  client.query(SQL, values)
-    .then(() => res.sendStatus(204))
-    .catch(console.error);
-});
-
-app.get('*', (req, res) => res.status(403).send('This route does not exist'));
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  return client.query(SQL, values);
+}
