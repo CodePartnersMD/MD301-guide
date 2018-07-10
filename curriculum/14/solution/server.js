20cf00fad599db09f61f76938e7533051ce86894
Allie <allie@codefellows.com>
2018-07-09 19:41:14 -0700

    Refactoring routes and callbacks

--- curriculum/14/solution/server.js
+++ curriculum/14/solution/server.js
@@ -13,74 +13,72 @@ const API_KEY = process.env.GOOGLE_API_KEY;
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-
-// Error handling
 client.on('error', err => console.error(err));
 
 // Application Middleware
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-// Serve static files
 app.use(express.static('public'));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
 // API Routes
-app.get('/api/v1/books', getBooks);
-
-app.get('/api/v1/books/find', searchForBook);
-
-app.get('/api/v1/books/add', showForm);
-
-app.get('/api/v1/books/:id', getOneBook);
-
-app.post('/api/v1/books/add', addBook);
-
-app.put('/api/v1/books/:id', updateBook);
-
-app.delete('/api/v1/books/:id', deleteBook);
+app.get('/books', getBooks);
+app.get('/books/new', newBook);
+app.get('/searches/new', newSearch);
+app.get('/books/:id', getBook);
+app.post('/books', createBook);
+app.post('/searches', createSearch);
+app.put('/books/:id', updateBook);
+app.delete('/books/:id', deleteBook);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-
 // HELPER FUNCTIONS
-
 function getBooks(request, response) {
   let SQL = 'SELECT * FROM books;';
 
   return client.query(SQL)
     .then(results => response.render('index', {books: results.rows}))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .catch(handleError);
 }
 
-function getOneBook(request, response) {
+function getBook(request, response) {
   let SQL = 'SELECT * FROM books WHERE id=$1;';
   let values = [request.params.id];
 
   return client.query(SQL, values)
-    .then(result => response.render('pages/detail-view', {book: result.rows[0]}))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(result => response.render('pages/books/show', {book: result.rows[0]}))
+    .catch(handleError);
 }
 
-function showForm(request, response) {
-  response.render('pages/add-view');
+function newBook(request, response) {
+  response.render('pages/books/new');
 }
 
-function addBook(request, response) {
+function newSearch(request, response) {
+  response.render('pages/searches/new');
+}
+
+function createBook(request, response) {
   let {title, author, isbn, image_url, description} = request.body;
   let SQL = 'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);';
   let values = [title, author, isbn, image_url, description];
 
   return client.query(SQL, values)
-    .then(response.redirect('/api/v1/books'))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(() => {
+      let SQL = 'SELECT * FROM books WHERE isbn=$1;';
+      let values = [request.body.isbn];
+      return client.query(SQL, values)
+        .then(result => response.render('pages/books/show', {book: result}))
+        .catch(handleError);
+    })
+    .catch(handleError);
 }
 
-function searchForBook(request, response) {
+function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes';
   let query = 'dune';
   // if(searchQuery.title) query += `+intitle:${searchQuery.title}`;
@@ -103,8 +101,8 @@ function searchForBook(request, response) {
         id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
       };
     }))
-    .then(results => response.render('pages/results-view', {results: results}))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(results => response.render('pages/searches/show', {results: results}))
+    .catch(handleError);
 }
 
 function updateBook(request, response) {
@@ -113,8 +111,8 @@ function updateBook(request, response) {
   let values = [title, author, isbn, image_url, description, request.params.id];
 
   return client.query(SQL, values)
-    .then(response.redirect('/api/v1/books'))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(response.render('pages/books/show'))
+    .catch(handleError);
 }
 
 function deleteBook(request, response) {
@@ -122,6 +120,10 @@ function deleteBook(request, response) {
   let values = [request.params.id];
 
   return client.query(SQL, values)
-    .then(response.redirect('/api/v1/books'))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(response.render('pages/books/show'))
+    .catch(handleError);
+}
+
+function handleError(error, response) {
+  response.render('pages/error-view', {error: error});
 }
