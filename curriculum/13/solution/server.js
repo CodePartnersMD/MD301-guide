f758c064b46671121911be99590e0eebea8fd31f
Allie <allie@codefellows.com>
2018-07-10 10:17:38 -0700

    Completes modifications to class-13 code, except styling

--- curriculum/13/solution/server.js
+++ curriculum/13/solution/server.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-// Application dependencies
+// Application Dependencies
 const express = require('express');
 const pg = require('pg');
 
@@ -11,36 +11,26 @@ const PORT = process.env.PORT;
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-
-// TODO: error handler elsewhere (with other functions below)
 client.on('error', err => console.error(err));
 
 // Application Middleware
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-// Serve static files
 app.use(express.static('public'));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
 // API Routes
-app.get('/api/v1/books', getBooks);
-
-app.get('/api/v1/books/:id', getOneBook);
-
-app.get('/api/v1/add', showForm);
-
-app.post('/api/v1/add', addBook);
+app.get('/books', getBooks);
+app.get('/books/new', newBook);
+app.get('/books/:id', getBook);
+app.post('/books', createBook);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-
 // HELPER FUNCTIONS
-
 function getBooks(request, response) {
   let SQL = 'SELECT * FROM books;';
 
@@ -49,26 +39,32 @@ function getBooks(request, response) {
     .catch(handleError);
 }
 
-function getOneBook(request, response) {
+function newBook(request, response) {
+  response.render('pages/books/new');
+}
+
+function getBook(request, response) {
   let SQL = 'SELECT * FROM books WHERE id=$1;';
   let values = [request.params.id];
 
   return client.query(SQL, values)
-    .then(result => response.render('pages/detail-view', {book: result.rows[0]}))
+    .then(result => response.render('pages/books/show', {book: result.rows[0], message: ''}))
     .catch(handleError);
 }
 
-function showForm(request, response) {
-  response.render('pages/add-view');
-}
-
-function addBook(request, response) {
+function createBook(request, response) {
   let {title, author, isbn, image_url, description} = request.body;
   let SQL = 'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);';
   let values = [title, author, isbn, image_url, description];
 
   return client.query(SQL, values)
-    .then(response.redirect('/api/v1/books'))
+    .then(() => {
+      SQL = 'SELECT * FROM books WHERE isbn=$1;';
+      values = [request.body.isbn];
+      return client.query(SQL, values)
+        .then(result => response.render('pages/books/show', {book: result.rows[0], message: 'This book has been added to your database!'}))
+        .catch(handleError);
+    })
     .catch(handleError);
 }
 
