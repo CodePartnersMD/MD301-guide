e29cae82ab76bf6b43b1638077174b620ce866d5
Allie <allie@codefellows.com>
2018-07-10 10:33:40 -0700

    Completes modifications to class-11 code, except styling

--- curriculum/11/solution/server.js
+++ curriculum/11/solution/server.js
@@ -1,8 +1,7 @@
-'use strict'
+'use strict';
 
-// Application dependencies
+// Application Dependencies
 const express = require('express');
-const cors = require('cors');
 const pg = require('pg');
 
 // Application Setup
@@ -15,28 +14,23 @@ client.connect();
 client.on('error', err => console.error(err));
 
 // Application Middleware
-app.use(cors());
-
-// Serve static files
 app.use(express.static('public'));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
 // API Routes
-app.get('/api/v1/books', getBooks);
+app.get('/books', getBooks);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-
 // HELPER FUNCTIONS
-
 function getBooks(request, response) {
-  let SQL = 'SELECT id, title, author, image_url, isbn FROM books;';
+  let SQL = 'SELECT * FROM books;';
 
   return client.query(SQL)
-    .then(results => response.render('index', {books: results.rows}))
-    .catch(error => response.render('pages/error-view', {error: error}));
+    .then(results => response.render('index', {results: results.rows}))
+    .catch(error => response.render('pages/error', {error: error}));
 }
