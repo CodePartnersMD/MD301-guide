e3aeea25d854a625259bee1bdda63143830c0657
Allie <allie@codefellows.com>
2018-07-22 10:02:02 -0700

    Storing dummy data for client buildout

--- curriculum/static-buildout/public/scripts/app.js
+++ curriculum/static-buildout/public/scripts/app.js
@@ -2,10 +2,16 @@
 
 $('#the-form').on('submit', fetchWeather);
 
+let tempData;
+
 function fetchWeather(event) {
   event.preventDefault();
   let data = $('#input-search').val();
   $.get('/test', {data: data})
-    .then(response => $('img').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${response.lat}%2c%20${response.lon}&zoom=13&size=600x300&maptype=roadmap
-    &key=${response.mapKey}`))
+    .then(response => {
+      console.log(response);
+      tempData = response;
+      $('img').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${response.lat}%2c%20${response.lon}&zoom=13&size=600x300&maptype=roadmap
+    &key=${response.mapKey}`)
+    })
 }
