3a5c10521c40489b3f1d6603f14d49e397a6e34b
Allie <allie@codefellows.com>
2018-07-25 05:37:31 -0700

    Initial styling

--- curriculum/static-buildout/public/scripts/app.js
+++ curriculum/static-buildout/public/scripts/app.js
@@ -22,6 +22,7 @@ function displayMap(location) {
   $('.query-placeholder').text(`Here are the results for ${location.search_query}`);
 
   $('#map').css('visibility', 'visible');
+  $('section').css('visibility', 'visible');
 
   $('#map').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude}%2c%20${location.longitude}&zoom=13&size=600x300&maptype=roadmap
   &key=AIzaSyDp0Caae9rkHUHwERAFzs6WN4_MuphTimk`)
