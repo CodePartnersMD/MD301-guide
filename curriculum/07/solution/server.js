0706bcffcf3d994009c7bc2dd0e58811ca70b48a
Allie <allie@codefellows.com>
2018-08-02 10:33:54 -0700

    Adds solution code for class 07

--- curriculum/07/solution/server.js
+++ curriculum/07/solution/server.js
@@ -4,19 +4,16 @@
 const express = require('express');
 const superagent = require('superagent');
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Application Setup
 const app = express();
 const PORT = 3000;
 
-// API keys
-const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-const YELP_API_KEY = process.env.YELP_API_KEY;
-const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
-
-// API routes
+// API Routes
 app.get('/location', (request, response) => {
-  stringToLatLong(request.query.data)
+  searchToLatLong(request.query.data)
     .then(location => response.send(location))
     .catch(error => handleError(error, response));
 })
@@ -28,35 +25,60 @@ app.get('/yelp', getYelp);
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// Helper Functions
-function stringToLatLong(query) {
-  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_API_KEY}`;
+// Error handler
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+// Models
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.body.result[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
+}
+
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+function Yelp(business) {
+  this.name = business.name;
+  this.image_url = business.image_url;
+  this.price = business.price;
+  this.rating = business.rating;
+  this.url = business.url;
+}
+
+function Movie(movie) {
+  this.title = movie.title;
+  this.overview = movie.overview;
+  this.average_votes = movie.vote_average;
+  this.total_votes = movie.vote_count;
+  this.image_url = 'https://image.tmdb.org/t/p/w200_and_h300_bestv2' + movie.poster_path;
+  this.popularity = movie.popularity;
+  this.released_on = movie.release_date;
+}
+
+function searchToLatLong(query) {
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
   return superagent.get(url)
     .then(res => {
-      let formattedQuery = formatQuery(res.body.results[0].address_components);
-      console.log(formattedQuery);
-      
-      return {
-        search_query: query,
-        formatted_query: formattedQuery,
-        latitude: res.body.results[0].geometry.location.lat,
-        longitude: res.body.results[0].geometry.location.lng
-      }
+      return new Location(query, res);
     })
     .catch(error => handleError(error));
 }
 
 function getWeather(request, response) {
-  let url = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  let url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
-  return superagent.get(url)
+  superagent.get(url)
     .then(result => {
       let weatherSummaries = result.body.daily.data.map(day => {
-        return {
-          forecast: day.summary,
-          time: new Date(day.time * 1000).toString().slice(0, 15)
-        }
+        return new Weather(day);
       });
 
       response.send(weatherSummaries);
@@ -67,17 +89,11 @@ function getWeather(request, response) {
 function getYelp(request, response) {
   let url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
 
-  return superagent.get(url)
-    .set('Authorization', `Bearer ${YELP_API_KEY}`)
+  superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .then(result => {
       let yelpSummaries = result.body.businesses.map(business => {
-        return {
-          name: business.name,
-          image_url: business.image_url,
-          price: business.price,
-          rating: business.rating,
-          url: business.url
-        };
+        return new Yelp(business);
       })
 
       response.send(yelpSummaries);
@@ -86,48 +102,15 @@ function getYelp(request, response) {
 }
 
 function getMovies(request, response) {
-  let url = `https://api.themoviedb.org/3/search/movie/?api_key=${MOVIE_API_KEY}&language=en-US&page=1&query=${request.query.data.search_query}`;
+  let url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&query=${request.query.data.search_query}`;
 
-  return superagent.get(url)
+  superagent.get(url)
     .then(result => {
-      let movieSummaries = result.body.results.map(movie => {
-        return {
-          title: movie.title,
-          overview: movie.overview,
-          averageVotes: movie.vote_average,
-          totalVotes: movie.vote_count,
-          image: 'https://image.tmdb.org/t/p/w200_and_h300_bestv2' + movie.poster_path,
-          popularity: movie.popularity,
-          releasedOn: movie.release_date
-        };
+      let movieSummaries = result.body.results.map( movie => {
+        return new Movie(movie);
       })
 
       response.send(movieSummaries);
     })
     .catch(error => handleError(error, response));
-}
-
-function handleError(err, res) {
-  console.error(err);
-  if (res) res.status(500).send('Sorry, something went wrong');
-}
-
-function formatQuery(searchDetails) {
-  let formattedQuery = '';
-
-  for(let i in searchDetails) {
-    if (searchDetails[i].types.includes('street_number')) formattedQuery += searchDetails[i].long_name += ' ';
-
-    if (searchDetails[i].types.includes('route')) formattedQuery += searchDetails[i].long_name += ', ';
-
-    if (searchDetails[i].types.includes('locality')) formattedQuery += searchDetails[i].long_name += ', ';
-
-    if (searchDetails[i].types.includes('administrative_area_level_1')) formattedQuery += searchDetails[i].long_name += ', ';
-
-    if (searchDetails[i].types.includes('country')) formattedQuery += searchDetails[i].long_name += ', ';
-
-    if (searchDetails[i].types.includes('postal_code')) formattedQuery += searchDetails[i].long_name+= ', ';
-  }
-
-  return formattedQuery.slice(0, -2);
 }
