1b25a067c3e712689fb149e7da730729ba9e76a8
Allie <allie@codefellows.com>
2018-08-22 10:30:31 -0700

    Basic functionality for lab 3 solution

--- curriculum/03-jquery-events/solution/app.js
+++ curriculum/03-jquery-events/solution/app.js
@@ -1,127 +1,114 @@
 'use strict';
 
-function Image(item) {
+function Image( item ) {
   this.image_url = item.image_url;
   this.title = item.title;
   this.description = item.description;
-  this.keyword1 = item.keyword1;
-  this.keyword2 = item.keyword2;
+  this.keyword = item.keyword;
   this.horns = item.horns;
 }
 
 Image.prototype.render = function() {
-  let template = Handlebars.compile($('#photo-template').html());
+  let template = Handlebars.compile( $( '#photo-template' ).html() );
   return template(this);
 }
 
-Image.readJson = (page) => {
+Image.readJson = ( page ) => {
   Image.all = [];
 
-  $('main div').empty();
+  $( 'main div' ).empty();
 
-  $.get(`page-${page}.json`).then(data => {
+  $.get( `page-${page}.json` ).then( data => {
 
-    data.forEach(item => {
-      Image.all.push(new Image(item));
+    data.forEach( item => {
+      Image.all.push( new Image( item ) );
     })
 
-    Image.sortBy(Image.all, 'title');
+    Image.sortBy( Image.all, 'title' );
 
-    Image.all.forEach(image => {
-      $('#image-container').append(image.render());
+    Image.all.forEach( image => {
+      $( '#image-container' ).append (image.render() );
     })
 
-  }, 'json')
-    .then(Image.populateFilter)
-    .then(Image.handleFilter)
-    .then(Image.handleSort)
-    .then(Image.detailView);
+  }, 'json' )
+    .then( Image.populateFilter )
+    .then( Image.handleFilter )
+    .then( Image.handleSort )
+    .then( Image.detailView );
 }
 
-Image.sortBy = function(array, property) {
-  array.sort((a,b) => {
+Image.sortBy = ( array, property ) => {
+  array.sort( ( a, b ) => {
     let firstComparison = a[property];
     let secondComparison = b[property];
-    if(firstComparison > secondComparison) return 1;
-    if(firstComparison < secondComparison) return -1;
-    return 0;
+    return ( firstComparison > secondComparison ) ? 1 : ( firstComparison < secondComparison ) ? -1 : 0;
   });
 }
 
-// $('input').on('change', Image.handleSort)
-
-// Image.handleSort = function() {
-//   $('div').remove()
-//   Image.sortBy(Image.all, $(this).attr('id'))
-//   Image.all.forEach(image => {
-//     $('#image-container').append(image.render());
-//   })
-// }
-
-Image.handleSort = function() {
-  $('input').on('change', function() {
-    $('div').remove()
-    Image.sortBy(Image.all, $(this).attr('id'))
-    Image.all.forEach(image => {
-      $('#image-container').append(image.render());
-    })
-  })
-}
-
-
-Image.populateFilter = function() {
+Image.populateFilter = () => {
   let filterKeywords = [];
 
   $('option').not(':first').remove();
 
   Image.all.forEach(image => {
-    if (!filterKeywords.includes(image.keyword1)) filterKeywords.push(image.keyword1);
-    if (!filterKeywords.includes(image.keyword2)) filterKeywords.push(image.keyword2);
-
+    if ( !filterKeywords.includes( image.keyword ) ) filterKeywords.push( image.keyword );
   })
-  filterKeywords.forEach(keyword => {
+
+  filterKeywords.sort();
+
+  filterKeywords.forEach( keyword => {
     let optionTag = `<option value="${keyword}">${keyword}</option>`;
-    $('select').append(optionTag);
+    $( 'select' ).append( optionTag );
   })
 }
 
 // $('select').on('change', Image.handleFilter);
 
-Image.handleFilter = function () {
-  $('select').on('change', function() {
-    if($(this).val() !== 'Filter By Keyword') {
-      $('div').hide();
+Image.handleFilter = () => {
+  $( 'select' ).on('change', function() {
+    let selected = $(this).val();
+    if( selected !== 'Filter By Keyword' ) {
+      $( 'div' ).hide();
 
       Image.all.forEach(image => {
-        if ($(this).val() === image.keyword1 || $(this).val() === image.keyword2) {
-          console.log('in')
-          console.log(`div[class="${$(this).val()} "]`);
-          $(`div[class="${$(this).val()} "`).fadeIn();
+        if ( selected === image.keyword ) {
+          $( `div[class="${selected} "` ).fadeIn();
         }
       })
 
-      $(`option[value=${$(this).val()}]`).fadeIn();
+      $( `option[value=${selected}]` ).fadeIn();
     }
   })
 }
 
-$('footer ul, header ul').on('click', 'li', function() {
-  Image.readJson($(this).attr('id'));
-})
+Image.handleSort = () => {
+  $( 'input' ).on('change', function() {
+    $('div').remove()
+    Image.sortBy( Image.all, $(this).attr('id') )
+    Image.all.forEach( image => {
+      $( '#image-container' ).append( image.render() );
+    })
+  })
+}
 
-Image.detailView = function() {
-  $('div').on('click', function() {
-    $('div:first-child').empty();
-    
+Image.detailView = () => {
+  $( 'div' ).on( 'click', function() {
+
+    // $( 'div:first-child' ).toggleClass('popout');
+    $( 'div:first-child' ).empty();
+
     let $clone = $(this).clone();
     let elements = $clone[0].children;
 
-    $('div:first-child').toggleClass('active').html(elements);
+    $( 'div:first-child' ).toggleClass( 'active' ).html( elements );
 
-    $(window).scrollTop(0);
+    $( window ).scrollTop( 0 );
   });
 }
 
+$( 'footer ul, header ul' ).on( 'click', 'li', function() {
+  $( '#image-container' ).empty();
+  Image.readJson( $(this).attr('id') );
+})
 
-Image.readJson(1);
-// Image.handleFilter();
+Image.readJson( 1 );
