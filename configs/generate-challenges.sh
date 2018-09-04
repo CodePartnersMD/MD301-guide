35c90366ecbf5fcd43e697b4521b8890bb20ed48
Brook Riggio <brook@rigg.io>
2018-09-04 15:56:56 -0700

    Update challenge generator to handle class numbers

--- configs/generate-challenges.sh
+++ configs/generate-challenges.sh
@@ -1,6 +1,28 @@
-replace_solutions(){
-  perl -0pe 's/<solution>[\s\S]*?<\/solution>/ Solution code here.../g' $1solutions.test.js > $1challenges.test.js
-}
-export -f replace_solutions
+# Execute this script from root of this repo: ./configs/generate-challenges.sh 
 
-ls -d ../curriculum/**/fundamentals/ | xargs -I % bash -c 'replace_solutions %'
+# Define a shell function we can invoke from a subshell
+replace_solutions(){
+  # Grab the first arg: the directory we are currently working in
+  dirName="$1" 
+  
+  # Extract class number from dir name, from pos 12, taking 2 chars. 
+  classNumber=${dirName:13:2} 
+
+  # Build up the new file name
+  newFile=$1challenges-$classNumber.test.js 
+
+  # Use Perl to do the substitution, and redirect output to a new file
+  perl -0pe 's/<solution>[\s\S]*?<\/solution>/ Solution code here.../g' $1solutions-$classNumber.test.js > $newFile 
+  
+  # Report on progress to user
+  echo "Generated class $classNumber challenge in $newFile" 
+}
+# Exporting the function makes it available to invoke
+export -f replace_solutions 
+
+# List all paths that include a 'challenges/' directory, and 
+# pipe the list xargs, to invoke our function once per directory
+ls -d ./curriculum/**/challenges/ | xargs -I % bash -c 'replace_solutions %'
+
+# Report to the user that we completed this script
+Echo "Done!"
